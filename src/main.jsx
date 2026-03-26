03c2408ecd06b85bdc9ac00ccc69ddc2227f3171
MANANAR2007 <mananar2007@gmail.com>
2026-03-26 09:51:33 +0530

    fix: remove basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/khushi-website">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
